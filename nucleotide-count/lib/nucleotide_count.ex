73889ec12eb7443352519415029b07ad4dd3074c
iamareebjamal <jamal.areeb@gmail.com>
2020-04-23 07:43:55 +0530

    feat(nucleotide-count): Implement

---
+++ nucleotide-count/lib/nucleotide_count.ex
@@ -0,0 +1,33 @@
+defmodule NucleotideCount do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a DNA strand.
+
+  ## Examples
+
+  iex> NucleotideCount.count('AATAA', ?A)
+  4
+
+  iex> NucleotideCount.count('AATAA', ?T)
+  1
+  """
+  @spec count(charlist(), char()) :: non_neg_integer()
+  def count(strand, nucleotide) do
+    Enum.filter(strand, fn x -> x == nucleotide end) |> Enum.count()
+  end
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> NucleotideCount.histogram('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec histogram(charlist()) :: map()
+  def histogram(strand) do
+    histogram = Map.new(@nucleotides, fn x -> {x, 0} end)
+    Enum.reduce(strand, histogram, fn x, map -> Map.update!(map, x, &(&1 + 1)) end)
+  end
+end
