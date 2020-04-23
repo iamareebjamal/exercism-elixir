07933919807ef6eb69f1ad7a8a61d5196372990a
iamareebjamal <jamal.areeb@gmail.com>
2020-04-23 07:59:26 +0530

    feat(nucleotide-count): Final improvement in histogram

--- nucleotide-count/lib/nucleotide_count.ex
+++ nucleotide-count/lib/nucleotide_count.ex
@@ -27,9 +27,6 @@ defmodule NucleotideCount do
   """
   @spec histogram(charlist()) :: map()
   def histogram(strand) do
-    # Can be done in a single line, but it is O(4N) instead of single pass
-    # Map.new @nucleotides, &{&1, count(strand, &1)}
-    histogram = Map.new(@nucleotides, &{&1, 0})
-    Enum.reduce(strand, histogram, fn x, map -> Map.update!(map, x, &(&1 + 1)) end)
+    Map.new(@nucleotides, &{&1, count(strand, &1)})
   end
 end
