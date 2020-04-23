72bd034d4832fedf16c92ab2e070782479b39c46
iamareebjamal <jamal.areeb@gmail.com>
2020-04-23 07:47:44 +0530

    feat(nucleotide-count): Improve count code

--- nucleotide-count/lib/nucleotide_count.ex
+++ nucleotide-count/lib/nucleotide_count.ex
@@ -14,7 +14,7 @@ defmodule NucleotideCount do
   """
   @spec count(charlist(), char()) :: non_neg_integer()
   def count(strand, nucleotide) do
-    Enum.filter(strand, fn x -> x == nucleotide end) |> Enum.count()
+    Enum.count(strand, &(&1 == nucleotide))
   end
 
   @doc """
